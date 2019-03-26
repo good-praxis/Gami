4b786163dc1b2534553dce418a60508f52f39170
Praxis | Darian <hello@vulpinecat.com>
2019-03-26 13:44:58 +0100

    add camera rotation

--- Assets/Scripts/PlayerCamera.cs
+++ Assets/Scripts/PlayerCamera.cs
@@ -5,29 +5,37 @@ using UnityEngine;
 public class PlayerCamera : MonoBehaviour
 {
     private Vector3 offset = Vector3.zero;
+    private float radius;
     private GameObject player;
 
     private Vector3 minOffset = new Vector3(-1000f, 3.3f, 0.1f);
     private Vector3 maxOffset = new Vector3(-2.2f, 1000f, 1000f);
-
+    private float angle = 0f;
 
     public int zoomMultiplier = 2; // Used to increase the zoom speed 
+    public int rotationMultiplier = 2; // Used to increase the speed of camera rotation
 
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
         offset = transform.position - player.transform.position;
+        radius = Mathf.Abs(offset.x) + Mathf.Abs(offset.z);
     }
 
     void Update()
     {
         float zoom = Input.GetAxis("Mouse ScrollWheel");
         transform.position = Vector3.MoveTowards(transform.position, player.transform.position, zoom * zoomMultiplier);
-        offset = clampCameraOffset(transform.position - player.transform.position);
+        offset = ClampCameraOffset(transform.position - player.transform.position);
         transform.position = player.transform.position + offset;
+
+        angle += Input.GetAxis("Horizontal") * rotationMultiplier * Time.deltaTime;
+        Vector2 rotation = new Vector2(Mathf.Sin(angle), Mathf.Cos(angle)) * radius;
+        transform.position = new Vector3(rotation.x, transform.position.y, rotation.y);
+        transform.LookAt(player.transform);
     }
 
-    Vector3 clampCameraOffset(Vector3 pos)
+    Vector3 ClampCameraOffset(Vector3 pos)
     {
         return new Vector3(
                 Mathf.Clamp(pos.x, minOffset.x, maxOffset.x),
