d4fa4cc3b6705a5723fcf45ba90a21923cf21094
hannesostwaldcode <hannes.ostwald@code.berlin>
2019-03-26 10:14:48 +0100

    Please Work?! There is nothing to see here, hopefully. If this is in the main branch, then we have a problem

---
+++ Assets/Scripts/Walk_simple.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Walk_simple : MonoBehaviour
+{
+    public float moveSpeed = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+
+    public void Movement()
+    {
+
+        float moveForward = Input.GetAxis("Vertical") * Time.deltaTime;
+        float moveSide = Input.GetAxis("Horizontal") * Time.deltaTime;
+
+        transform.Translate(Vector3.forward * moveForward * moveSpeed);
+        transform.Translate(Vector3.right * moveSide * moveSpeed);
+    }
+}
